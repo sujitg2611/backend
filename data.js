24d0c13047eba064ebccfa36e365f2a6ad742a8f
jitu712 <jitu712@gmail.com>
2022-10-19 19:34:47 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,74 @@
+import bcrypt from 'bcryptjs';
+
+const data = {
+  users: [
+    {
+      name: 'Sujit',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      // _id: '1',
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim;shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      // _id: '2',
+      name: 'Nike Slim shirt2',
+      slug: 'nike-slim;shirt3',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      // _id: '3',
+      name: 'Nike Slim shirt4',
+      slug: 'Uspolo-slim;shirt5',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      // _id: '4',
+      name: 'Nike Slim shirt6',
+      slug: 'Arrow-slim;shirt7',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+  ],
+};
+
+export default data;
